3f08cb7383045b36bb629b5f7bfb058146061b3f
m1016m <m1016m@gmail.com>
2025-05-31 12:11:02 +0800

    Add files via upload

--- splint_project/settings.py
+++ splint_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+SQLITE_DB_PATH = os.environ.get('SQLITE_DB_PATH', BASE_DIR / 'db.sqlite3') 
 ROOT_URLCONF = 'splint_project.urls'
 
 TEMPLATES = [
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'splint_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/data/db.sqlite3', # 假設 /var/data 是你掛載的持久化磁碟
+        'NAME': SQLITE_DB_PATH, # 使用這個變數
     }
 }
 
